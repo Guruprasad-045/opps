88811c1e2a7e8b8cdcfb9e9ae3447d9f0d6fcbb7
Guruprasad <guruprasad79933@gmail.com>
2024-12-04 23:40:22 +0530

    Update encapsulationexample.java

--- encapsulationexample.java
+++ encapsulationexample.java
@@ -25,3 +25,20 @@ class encapsulationexample {
 		}
 }
 
+public class main {
+
+	
+	public static void main(String[] args) {
+
+			encapsulationexample c=new encapsulationexample();
+			c.setId(1);
+			c.setName("guru");
+			c.setNum(887665435);
+			System.out.println(c.getId());
+			System.out.println(c.getName());
+			System.out.println(c.getNum());
+		
+	}
+}
+
+
